7ee1f1d6d2fdea98b670f8d36e2c11ff42eda5cd
DiegoRivera <dr6937999@gmail.com>
2025-08-15 09:52:58 -0600

    ejercicio1

---
+++ mayor.py
@@ -0,0 +1,7 @@
+numero1 = int(input("ingrese el numero 1:"))
+numero2 = int(input("ingrese el numero 2:"))
+
+if numero1 > numero2 :
+    print("el numero mayor es: ",numero1)
+else:
+    print("el numero mayor es: ",numero2)
