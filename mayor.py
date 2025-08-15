b3bb641595e085c6a2254c65092e90881da68671
DiegoRivera <dr6937999@gmail.com>
2025-08-15 10:11:50 -0600

    mini calculadora

--- mayor.py
+++
@@ -1,7 +0,0 @@
-numero1 = int(input("ingrese el numero 1:"))
-numero2 = int(input("ingrese el numero 2:"))
-
-if numero1 > numero2 :
-    print("el numero mayor es: ",numero1)
-else:
-    print("el numero mayor es: ",numero2)
