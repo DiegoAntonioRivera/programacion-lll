d4ff19567436fad11a8fd3689751e5f052ea402c
DiegoRivera <dr6937999@gmail.com>
2025-08-22 09:59:31 -0600

    funciones01

---
+++ funciones01.py
@@ -0,0 +1,10 @@
+#crear la funcion
+def suma(num1, num2):
+    return num1 + num2
+
+#solicitar los numeros
+num1 = int(input("intruduce el numero 1: "))
+num2 = int(input("intruduce el numero 2: "))
+
+#imprimir el resultado
+print(f"la suma de{num1} y {num2} es {suma(num1, num2)}")4
